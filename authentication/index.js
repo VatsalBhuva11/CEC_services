8afec552761d8691c415731bf3056279969cdcfd
Vatsal Bhuva <vatsalbhuva11@gmail.com>
2025-04-09 03:55:34 +0530

    compose file

--- authentication/index.js
+++ authentication/index.js
@@ -156,8 +156,11 @@ app.post("/verify", async (req, res) => {
 });
 
 app.post("/verifyRole", async (req, res) => {
-    const token = req.headers.authorization?.split(" ")[1];
+    const token = req.headers?.authorization?.split(" ")[1];
     const { roles } = req.body;
+    console.log("Coming here!");
+    console.log("token: ", token);
+    console.log("roles: ", roles);
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     try {
